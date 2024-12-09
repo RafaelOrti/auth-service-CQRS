811a36b6e29c8bdb60d212ab8cafddb4bbe56b76
Rafa <raorcar3@gmail.com>
2024-12-09 18:21:34 +0100

    Modify auth service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 // import {OpiModule}  from './opi/opi.module'; // Asegúrate de que la ruta sea correcta
-import { OrderModule } from './order/order.module'; // Asegúrate de que la ruta sea correcta
+// import { OrderModule } from './order/order.module'; // Asegúrate de que la ruta sea correcta
 
 @Module({
   imports: [
     AuthModule,
     // OpiModule,
-    OrderModule,
+    // OrderModule,
   ],
 })
 export class AppModule {}
